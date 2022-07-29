07b3d0a4bf3285ed884c08f2ee9c54206cf07c39
davidheweb <davidhermosilladuro@gmail.com>
2022-07-29 21:46:14 +0200

    Sending and handling a POST ajax request

--- public/scripts/comments.js
+++ public/scripts/comments.js
@@ -1,11 +1,14 @@
 const loadCommentsBtnElement = document.getElementById("load-comments-btn");
 const commentsSectionElement = document.getElementById("comments");
+const commentsFormElement = document.querySelector("#comments-form form");
+const commentTitleElement = document.getElementById("title");
+const commentTextElement = document.getElementById("text");
 
 function createCommentsList(comments) {
   const commentListElement = document.createElement("ol");
 
   for (const comment of comments) {
-    const commentElement = document.createElement("li")
+    const commentElement = document.createElement("li");
     commentElement.innerHTML = `
       <article class="comment-item">
         <h2>${comment.title}</h2>
@@ -28,4 +31,23 @@ async function fetchCommentsForPost() {
   commentsSectionElement.append(commentsListElement);
 }
 
+function saveComment(event) {
+  event.preventDefault();
+  const postId = commentsFormElement.dataset.postid;
+
+  const enteredTitle = commentTitleElement.value;
+  const enteredText = commentTextElement.value;
+
+  const comment = { title: enteredTitle, text: enteredText };
+
+  fetch(`/posts/${postId}/comments`, {
+    method: "POST",
+    body: JSON.stringify(comment),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
 loadCommentsBtnElement.addEventListener("click", fetchCommentsForPost);
+commentsFormElement.addEventListener("submit", saveComment);
